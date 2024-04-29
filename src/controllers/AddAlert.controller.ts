8a0f83a57fe18cf061dcfa1e720430b92a08b5f8
onfranciis <onukwuf@gmail.com>
2024-04-29 19:33:29 +0100

    feat: create AddAlert controller

---
+++ src/controllers/AddAlert.controller.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from "express";
+import { AddAlertValidator } from "../utils/AddAlertValidator.util";
+import Alert from "../models/Alert.model";
+
+export const AddAlert: RequestHandler = async (req, res) => {
+  try {
+    const body = req.body;
+
+    AddAlertValidator(body)
+      .then(async (form) => {
+        const result = await Alert.create({
+          message: form.message,
+          location: form.location,
+        });
+
+        result["__v"] = undefined;
+
+        return res.send({ error: null, result });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(400).send({ result: null, error: err });
+      });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ result: null, error: `Something went wrong!` });
+  }
+};
