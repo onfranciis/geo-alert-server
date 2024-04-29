9e95047bf8a6eedea8fcf1d530e28e6c3c418c3f
onfranciis <onukwuf@gmail.com>
2024-04-29 21:31:27 +0100

    fix: add render support

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,14 @@ if (Object.values(config).includes("")) {
   console.error(`It seems like your environment variables are not complete.\n\
   Kindly check and restart the server.`);
   process.exit(1);
-}
-
-connect(config.dbUrl)
-  .then(() => {
-    app.listen(config.port, () => {
-      console.log(`Server has started running on port ${config.port} ...`);
+} else {
+  connect(config.dbUrl)
+    .then(() => {
+      app.listen(config.port, () => {
+        console.log(`Server has started running on port ${config.port} ...`);
+      });
+    })
+    .catch((err) => {
+      console.log(`Something went wrong\n`, err);
     });
-  })
-  .catch((err) => {
-    console.log(`Something went wrong\n`, err);
-  });
+}
